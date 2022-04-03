5fc9240d710a5aa03796981a2ba2f38faad61ab5
Geovana Oliveira <ogeovana427@gmail.com>
2022-04-03 19:41:55 -0300

    completei-e-adicionei-o-projeto-blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -17,13 +17,57 @@
 //Regras: Recebendo 2 cartas - A pontuação de cada jogador é a soma do valor das suas cartas.
 //O jogador com a maior pontuação ganha a rodada.
 
-//1.
-const imrimeBoasVindas = "Boas vindas ao jogo de Blackjack!"
-console.log (imrimeBoasVindas)
 
-//2.
-   if (confirm(prompt("Quer iniciar uma nova rodada?")) {
-      console.log("O jogo acabou")
+const imprimeBoasVindas = "Boas vindas ao jogo de Blackjack!"
+console.log (imprimeBoasVindas)
+
+   if (confirm("Quer iniciar uma nova rodada?") === true) {
+      var baralho = [
+         {texto: "Ás♠", valor: 11}, 
+         {texto: "Ás♣", valor: 11},
+         {texto: "Ás♥", valor: 11},
+         {texto: "Ás♦", valor: 11},
+         //...Continua todas as cartas
+      ] 
+      let cartaUsuario1 = {valorTotal: 0, cartas: []}
+      let cartaUsuario2 = {valorTotal: 0, cartas: []}
+      let cartaPc1 = {valorTotal: 0, cartas: []}
+      let cartaPc2 = {valorTotal: 0, cartas: []}
+
+      cartaUsuario1.cartas.push(comprarCarta())
+      cartaUsuario1.valorTotal = calcularValorCartas(cartaUsuario1.cartas)
+      console.log("Usuário 1" + " - cartas:" + listarCartas(cartaUsuario1.cartas) + "- pontuação" + cartaUsuario1.valorTotal)
+      
+      cartaUsuario2.cartas.push(comprarCarta())
+      cartaUsuario2.valorTotal = calcularValorCartas(cartaUsuario2.cartas)
+      console.log("Usuário 2" + " - cartas:" + listarCartas(cartaUsuario2.cartas) + "- pontuação" + cartaUsuario2.valorTotal)
+
    }else{
-      console.log()
+      console.log("O jogo acabou.")
    }
+
+
+function listarCartas (cartas) {
+   let cartaNomes = ""
+   for(let carta of cartas){
+      cartaNomes += carta.texto + ", "
+   } 
+   return cartaNomes
+}
+
+function calcularValorCartas (cartas) {
+   let valor = 0
+   for(let carta of cartas){
+      valor += carta.valor
+   }
+   return valor
+}
+
+function comprarCarta () {
+   let index = Math.floor(Math.random() * (baralho.length));
+   console.log(this.baralho)
+   const carta = baralho[index]
+   baralho.splice(index, 1)
+   console.log(carta, index)
+   return carta
+}
