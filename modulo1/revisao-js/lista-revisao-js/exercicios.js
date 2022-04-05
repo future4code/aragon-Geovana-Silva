85939b8330d0e9621f5d5514d536b8c9dec4f7b6
Geovana Oliveira <ogeovana427@gmail.com>
2022-04-04 21:01:33 -0300

    adicionei-o-retornaPessoasAutorizadas

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -77,7 +77,20 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-
+    const pessoas = [
+        { nome: "Paula", idade: 12, altura: 1.8},
+        { nome: "João", idade: 20, altura: 1.3},
+        { nome: "Pedro", idade: 15, altura: 1.9},
+        { nome: "Luciano", idade: 22, altura: 1.8},
+        { nome: "Artur", idade: 10, altura: 1.2},
+        { nome: "Soter", idade: 70, altura: 1.9}
+    ]
+    const altura = pessoas.map((alturaMin) => {
+        return {
+            nome: pessoas.altura >= 1.5
+        }
+    })
+    return altura
 }
 
 // EXERCÍCIO 13B
