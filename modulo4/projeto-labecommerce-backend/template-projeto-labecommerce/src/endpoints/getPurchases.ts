dfcf9702a809c6bae1a114850bfc307c130519d2
Geovana Oliveira <ogeovana427@gmail.com>
2022-07-22 17:45:00 -0300

    continuacao-projeto

--- modulo4/projeto-labecommerce-backend/template-projeto-labecommerce/src/endpoints/getPurchases.ts
+++ modulo4/projeto-labecommerce-backend/template-projeto-labecommerce/src/endpoints/getPurchases.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import connection from "../database/connection";
-import { TABLE_PURCHASES } from "../database/tableNames";
+import { TABLE_PRODUCTS, TABLE_PURCHASES, TABLE_USERS } from "../database/tableNames";
 
 export const getPurchases = async (req: Request, res: Response) => {
     let errorCode = 400
@@ -16,15 +16,23 @@ export const getPurchases = async (req: Request, res: Response) => {
             errorCode = 422
             throw new Error(`User Id doesn't exist! Please, inform it!`)
         }
-
+        
         if(user_id){
-            const result = await connection(TABLE_PURCHASES)
-                .select()
-                .where("user_id", "LIKE", `%${user_id}%`)
-                .orderBy(`${sort}`, `${order}`)
-                .limit(limit)
-                .offset(offset)
-            return res.status(200).send({ users: result })
+            const result = await connection.raw(`
+                SELECT
+                ${TABLE_USERS}.email,
+                ${TABLE_PRODUCTS}.name,
+                ${TABLE_PRODUCTS}.price,
+                ${TABLE_PURCHASES}.quantity,
+                ${TABLE_PURCHASES}.total_price
+                FROM ${TABLE_PURCHASES}
+                JOIN ${TABLE_USERS}
+                ON ${TABLE_PURCHASES}.user_id = ${TABLE_USERS}.id
+                JOIN ${TABLE_PRODUCTS}
+                ON ${TABLE_PURCHASES}.product_id = ${TABLE_PRODUCTS}.id
+                WHERE ${TABLE_PURCHASES}.user_id = ${user_id};
+            `)
+            res.status(200).send({ users: result })
         }
 
         const result = await connection(TABLE_PURCHASES)
